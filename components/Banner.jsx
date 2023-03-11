871364aaebfaffe48a6406dd1f2543a96aff058a
Bhavya Gor <bhavya.gor9999@gmail.com>
2023-03-11 16:12:03 +0530

    changes

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -2,8 +2,19 @@ import React from "react";
 import hero from "../assets/Rectangle.png";
 const Banner = () => {
   return (
-    <div class="p-16 md:pt-0 md:p-0 w-full ">
-      <div className="bg-[url('../assets/banner.svg')] h-[50vh]"></div>
+    <div class="md:pt-0 md:p-0 w-full bg-[#1A73E8]  ">
+      <div className="flex items-center justify-between">
+        <img
+          src="https://www.gstatic.com/mobilesdk/190805_mobilesdk/woman-at-desktop@2x.png"
+          alt=""
+          className="h-[40vh] p-4"
+        />
+        <img
+          src="https://www.gstatic.com/mobilesdk/190805_mobilesdk/man-at-desktop@2x.png"
+          alt=""
+          className="h-[40vh] p-4"
+        />
+      </div>
     </div>
   );
 };
--- utils/firebase-config.js
+++ utils/firebase-config.js
@@ -9,14 +9,15 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyDfsrrs6RNQVJ9qaeOtGn837o4XToUWfP0",
   authDomain: "coupon-generator-a00eb.firebaseapp.com",
-  databaseURL: "https://coupon-generator-a00eb-default-rtdb.asia-southeast1.firebasedatabase.app",
+  databaseURL:
+    "https://coupon-generator-a00eb-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "coupon-generator-a00eb",
   storageBucket: "coupon-generator-a00eb.appspot.com",
   messagingSenderId: "65962690018",
-  appId: "1:65962690018:web:294b4ae789709339e0a5bb"
+  appId: "1:65962690018:web:294b4ae789709339e0a5bb",
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
